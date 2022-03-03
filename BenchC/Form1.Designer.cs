1a636eee4d6e125cbee76706fd64681300f24958
Krystofer Lando <krystofer02@gmail.com>
2022-03-04 00:15:17 +0100

    first&last commit

---
+++ BenchC/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace BenchC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.input_btn = new System.Windows.Forms.Button();
+            this.input_textbox = new System.Windows.Forms.TextBox();
+            this.output_textbox = new System.Windows.Forms.TextBox();
+            this.output_btn = new System.Windows.Forms.Button();
+            this.spakuj_btn = new System.Windows.Forms.Button();
+            this.open_dir_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plik lub Folder do kompresji";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Plik wynikowy";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(13, 105);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(764, 11);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // input_btn
+            // 
+            this.input_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.input_btn.Location = new System.Drawing.Point(716, 22);
+            this.input_btn.Name = "input_btn";
+            this.input_btn.Size = new System.Drawing.Size(72, 23);
+            this.input_btn.TabIndex = 3;
+            this.input_btn.Text = "Wybierz";
+            this.input_btn.UseVisualStyleBackColor = true;
+            this.input_btn.Click += new System.EventHandler(this.input_btn_Click);
+            // 
+            // input_textbox
+            // 
+            this.input_textbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.input_textbox.Enabled = false;
+            this.input_textbox.Location = new System.Drawing.Point(173, 22);
+            this.input_textbox.Name = "input_textbox";
+            this.input_textbox.Size = new System.Drawing.Size(537, 23);
+            this.input_textbox.TabIndex = 4;
+            // 
+            // output_textbox
+            // 
+            this.output_textbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.output_textbox.Enabled = false;
+            this.output_textbox.Location = new System.Drawing.Point(173, 53);
+            this.output_textbox.Name = "output_textbox";
+            this.output_textbox.Size = new System.Drawing.Size(537, 23);
+            this.output_textbox.TabIndex = 5;
+            // 
+            // output_btn
+            // 
+            this.output_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.output_btn.Location = new System.Drawing.Point(716, 53);
+            this.output_btn.Name = "output_btn";
+            this.output_btn.Size = new System.Drawing.Size(72, 23);
+            this.output_btn.TabIndex = 6;
+            this.output_btn.Text = "Wybierz";
+            this.output_btn.UseVisualStyleBackColor = true;
+            this.output_btn.Click += new System.EventHandler(this.output_btn_Click);
+            // 
+            // spakuj_btn
+            // 
+            this.spakuj_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.spakuj_btn.Location = new System.Drawing.Point(716, 128);
+            this.spakuj_btn.Name = "spakuj_btn";
+            this.spakuj_btn.Size = new System.Drawing.Size(72, 28);
+            this.spakuj_btn.TabIndex = 7;
+            this.spakuj_btn.Text = "Spakuj";
+            this.spakuj_btn.UseVisualStyleBackColor = true;
+            this.spakuj_btn.Click += new System.EventHandler(this.spakuj_btn_Click);
+            // 
+            // open_dir_btn
+            // 
+            this.open_dir_btn.Location = new System.Drawing.Point(14, 131);
+            this.open_dir_btn.Name = "open_dir_btn";
+            this.open_dir_btn.Size = new System.Drawing.Size(153, 23);
+            this.open_dir_btn.TabIndex = 8;
+            this.open_dir_btn.Text = "Otwórz folder wynikowy";
+            this.open_dir_btn.UseVisualStyleBackColor = true;
+            this.open_dir_btn.Click += new System.EventHandler(this.open_dir_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 166);
+            this.Controls.Add(this.open_dir_btn);
+            this.Controls.Add(this.spakuj_btn);
+            this.Controls.Add(this.output_btn);
+            this.Controls.Add(this.output_textbox);
+            this.Controls.Add(this.input_textbox);
+            this.Controls.Add(this.input_btn);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button input_btn;
+        private System.Windows.Forms.TextBox input_textbox;
+        private System.Windows.Forms.TextBox output_textbox;
+        private System.Windows.Forms.Button output_btn;
+        private System.Windows.Forms.Button spakuj_btn;
+        private System.Windows.Forms.Button open_dir_btn;
+    }
+}
+
---
+++ BenchC/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace BenchC
+{
+    public partial class Form1 : Form
+    {
+        bool poinformowano = false;
+        bool gotowe = false;
+        Compression compression;
+        public Form1()
+        {
+            InitializeComponent();
+            this.compression = new Compression();
+            compression.ProgressChanged += this.ChangeProgressBar;
+        }
+
+        private void input_btn_Click(object sender, EventArgs e)
+        {
+            // Otwórz okno dialogowe z możliwością wyboru pliku lub folderu.
+            FileDialog fd = new OpenFileDialog();
+            fd.ValidateNames = false;
+            fd.CheckFileExists = false;
+            fd.CheckPathExists = true;
+            fd.FileName = "wybór folderu";
+            if(!poinformowano)
+                MessageBox.Show("Aby wybrać folder wpisz nazwę nieistniejącego pliku.");
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(fd.FileName))
+                {
+                    compression.SetInputPath(fd.FileName);
+                    input_textbox.Text = fd.FileName;
+                }
+                else
+                {
+                    string fp = Path.GetDirectoryName(fd.FileName);
+                    compression.SetInputPath(fp, true);
+                    input_textbox.Text = fp;
+                }
+                poinformowano = true;
+                gotowe = false;
+            }
+        }
+
+        private void output_btn_Click(object sender, EventArgs e)
+        {
+            FileDialog fd = new SaveFileDialog();
+            fd.Filter = "Pliki archiwum | *.7z, *.zip";
+            if(fd.ShowDialog() == DialogResult.OK)
+            {
+                compression.OutputPath = fd.FileName;
+                output_textbox.Text = fd.FileName;
+                gotowe = false;
+            }
+        }
+
+        private void spakuj_btn_Click(object sender, EventArgs e)
+        {
+            if (compression.Ready())
+            {
+                string k = compression.checkDiversity();
+                compression.SetMethodAndFormat(k);
+                compression.Compress();
+                gotowe = true;
+            }
+        }
+
+        private void open_dir_btn_Click(object sender, EventArgs e)
+        {
+            if (!gotowe) return;
+            using (Process p = new Process())
+            {
+                p.StartInfo.FileName = "explorer";
+                p.StartInfo.Arguments = Path.GetFullPath(Path.GetDirectoryName(compression.OutputPath));
+                p.Start();
+            }
+        }
+        public void ChangeProgressBar(object sender, ProgressChangedEventArgs args)
+        {
+            progressBar1.Value = args.ProgressPercentage;
+        }
+    }
+    class Compression
+    {
+        string output_path, input_path;
+        bool directory;
+        string method, format; // format = ext.
+        int progress;
+
+        public event ProgressChangedEventHandler ProgressChanged;
+
+        public Compression()
+        {
+            output_path = input_path = method = format = string.Empty;
+            directory = false;
+            progress = 0;
+        }
+        public void SetInputPath(string value, bool dir = false)
+        {
+            input_path = value;
+            directory = dir;
+        }
+        public string GetInputPath() { return input_path; }
+        public string OutputPath
+        {
+            set{ output_path = value; }
+            get { return output_path;  }
+        }
+        public bool Ready()
+        {
+            if (output_path != string.Empty && input_path != string.Empty)
+                return true;
+            else
+                return false;
+        }
+        public int Progress
+        {
+            get
+            {
+                return progress;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                    progress = value;
+                try { ProgressChanged.Invoke(this, new ProgressChangedEventArgs(Progress, null)); } catch { }
+            }
+        }
+        void SMF(string m, string f)
+        {
+            method = m;
+            format = f;
+        }
+        public string checkDiversity()
+        {
+            if(directory)
+                return checkDiversity(input_path);
+            else
+            {
+                string ext = Path.GetExtension(input_path);
+                if (Regex.IsMatch(ext, "bin|txt"))
+                    return "binary, tekst";
+                else if (Regex.IsMatch(ext, "jpg|bmp|png"))
+                    return "images";
+                else if (Regex.IsMatch(ext, "mp3|wav|ogg|mp4|avi|mpg"))
+                    return "audio, video";
+                else
+                    return "bin,txt";
+            }
+        }
+        public string checkDiversity(string dir)
+        {
+            Hashtable tof = new Hashtable()
+            {
+                {"bin,txt", false },
+                {"audio,video", false},
+                {"hybrid", false},
+                {"images", false}
+            };
+            foreach (string file in Directory.EnumerateFiles(dir, "*"))
+            {
+                string ext = Path.GetExtension(file);
+                if (Regex.IsMatch(ext, "bin|txt"))
+                {
+                    tof["bin,txt"] = true;
+                    Console.WriteLine("binary, text");
+                }
+                else if (Regex.IsMatch(ext, "jpg|bmp|png"))
+                {
+                    tof["images"] = true;
+                    Console.WriteLine("images");
+                }
+                else if (Regex.IsMatch(ext, "mp3|wav|ogg|mp4|avi|mpg"))
+                {
+                    tof["audio,video"] = true;
+                    Console.WriteLine("audio, video");
+                }
+                else
+                {
+                    tof["bin,txt"] = true;
+                    Console.WriteLine("brak rozszerzenia np.");
+                }
+            }
+            int count = 0;
+            foreach (string b in tof.Keys)
+            {
+                if ((bool)tof[b] == true) ++count;
+            }
+            if (count > 1)
+            {
+                return "hybrid";
+            }
+            foreach (string dirn in Directory.EnumerateDirectories(dir, "*"))
+            {
+                try
+                {
+                    if ((bool)tof[checkDiversity(dirn)] != true)
+                    {
+                        return "hybrid";
+                    }
+                }
+                catch (KeyNotFoundException exception) { }
+            }
+            // Sprawdź dla którego klucza jest wartość true i zwróć ten klucz;
+            foreach (string b in tof.Keys) if ((bool)tof[b] == true) return b;
+            return ""; // brak plików.
+        }
+        public void SetMethodAndFormat(string kind) 
+        {
+            if (kind == "images") SMF("LZMA","7z");
+            else if(kind =="audio,video") SMF("Deflate","zip");
+            else if (kind == "bin,txt") SMF("Deflate","zip");
+            else if (kind == "hybrid") SMF("Deflate","zip");
+        }
+        void SeekProgress(object sender, DataReceivedEventArgs arg)
+        {
+            if (arg.Data != null)
+            {
+                Match g = Regex.Match(arg.Data, "\\d{1,3}%");
+                if (g.Success)
+                {
+                    Progress = int.Parse(g.Value.Trim('%'));
+                }
+            }
+        }
+        public void Compress()
+        {
+            Progress = 0;
+            string program = "\"C:\\Program Files\\7-Zip\\7z.exe\"";
+            string args = "a -m9=" + method + " -bsp2 \"" + Path.ChangeExtension(output_path, format) +
+                "\" \"" + input_path + "\"";
+            using (Process p = new Process())
+            {
+                p.StartInfo.FileName = program;
+                p.StartInfo.Arguments = args;
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.ErrorDataReceived += this.SeekProgress;
+                p.Start();
+                p.BeginErrorReadLine();
+                p.WaitForExit();
+                Progress = 100;
+            }
+        }
+    }
+}
